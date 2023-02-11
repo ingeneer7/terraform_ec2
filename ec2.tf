20402e0d5035da8014c6aaf7902da7ded0051b63
ingeneer7 <52728909+ingeneer7@users.noreply.github.com>
2023-02-11 16:27:22 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,8 @@
 #Custom Terraform module for EC2
 module "root_module" {
     source = "./child_module"
+    ami           = var.ami
+    instance_type = var.instance
+    value       = aws_instance.ec2_instance.public_ip
 }
 

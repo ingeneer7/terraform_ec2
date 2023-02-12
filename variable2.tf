735bbafc0ca4390920c7175f5314da66c140d34c
ingeneer7 <52728909+ingeneer7@users.noreply.github.com>
2023-02-11 23:03:35 -0500

    Update variable2.tf

--- variable2.tf
+++ variable2.tf
@@ -1,3 +1,12 @@
 #Root Module variables
-variable "ami" {}
-variable "instance" {}
+variable "ami" {
+  description = "The AMI ID of the instance"
+  type        = string
+  default     = "ami-0aa7d40eeae50c9a9"
+}
+
+variable "instance" {
+  description = "The Instance type of EC2"
+  type        = string
+  default     = "t2.micro"
+}
